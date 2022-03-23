44cb4f5e6175a3364c7bbe0190b3bf4ffca27c9e
Se-young Hwang <nahwasa@gmail.com>
2022-03-23 11:28:37 +0900

    project init using 'spring initializr'

---
+++ src/main/java/com/nahwasa/toy/toyreactivespringbackend/ToyReactiveSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nahwasa.toy.toyreactivespringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyReactiveSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyReactiveSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nahwasa/toy/toyreactivespringbackend/ToyReactiveSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahwasa.toy.toyreactivespringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyReactiveSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
